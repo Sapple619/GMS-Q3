8436560c86ff3f9d99031e9727fd7581f498f3ef
Sapple619 <ardor6191@naver.com>
2023-09-26 20:21:56 +0900

    Inital commit

---
+++ Assets/Scripts/Main/Door.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public bool IsLocked;
+    [SerializeField] private GameObject InsideObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        IsLocked = false;
+    }
+
+    // Update is called once per frame
+    public void Open()
+    {
+        if(!IsLocked){
+            InsideObject.SetActive(true);
+            this.gameObject.SetActive(false);
+        }
+    }
+}
