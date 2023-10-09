d8dfe6e21dcef2386d00b2bcb905ed515af7ef71
Mlnln <khmin.usa@gmail.com>
2023-10-09 15:31:30 +0900

    Make lock&safe Puzzles clear

---
+++ Assets/Scripts/Main/Locker.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class Locker : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI LKtxt;
+    public int LK;
+    public void LKP(){
+        if(LK!=10){
+            LK = LK+1;
+        }
+        if(LK==10){
+            LK=1;
+        }
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        LK = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LKtxt.text = ""+LK;
+    }
+}
